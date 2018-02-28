096a22c1166085162c0347d061314d37b65649fc
lishuaihua <lishuaihua@ejoyst.com>
2018-02-28 18:38:34 +0800

    接待界面完成

--- app/src/main/java/com/yijian/staff/mvp/reception/ReceptionActivity.java
+++ app/src/main/java/com/yijian/staff/mvp/reception/ReceptionActivity.java
@@ -3,14 +3,27 @@ package com.yijian.staff.mvp.reception;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.yijian.staff.R;
+import com.yijian.staff.util.Logger;
 import com.yijian.staff.util.system.StatusBarUtil;
 import com.yijian.staff.widget.NavigationBar;
 import com.yijian.staff.widget.NavigationBarItemFactory;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReceptionActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private List<ReceptionInfo> mReceptionInfoList=new ArrayList<>();
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +31,45 @@ public class ReceptionActivity extends AppCompatActivity implements View.OnClick
         setContentView(R.layout.activity_reception);
         StatusBarUtil.setLightStatusBar(this, 0xffffff);
 
+        initView();
+
+
+    }
+
+    private void initView() {
         NavigationBar navigationBar = (NavigationBar) findViewById(R.id.reception_activity_navigation_bar);
         navigationBar.setTitle("接待", "#000000");
         navigationBar.setNavigationBarBackgroudColor(Color.parseColor("#ffffff"));
         navigationBar.setLeftButtonView(NavigationBarItemFactory.createNavigationItemImageView(this, NavigationBarItemFactory.NavigationItemType.BACK_BLACK));
         navigationBar.setLeftButtonClickListener(NavigationBarItemFactory.createBackClickListener(this));
+
+        recyclerView = findViewById(R.id.recyclerview_jiedai_history);
+
+
+
+        JSONObject jsonObject=new JSONObject();
+        try {
+            jsonObject.put("name","朱沙");
+            jsonObject.put("phone","12345678900");
+            jsonObject.put("sex","男");
+            jsonObject.put("status","完成");
+            jsonObject.put("product","十周年纪念卡");
+            for (int i = 0; i < 10; i++) {
+                ReceptionInfo receptionInfo = new ReceptionInfo(jsonObject);
+                mReceptionInfoList.add(receptionInfo);
+            }
+            Logger.i("TEST","mReceptionInfoList: "+mReceptionInfoList.size());
+
+
+            LinearLayoutManager layoutmanager = new LinearLayoutManager(this);
+            //设置RecyclerView 布局
+            recyclerView.setLayoutManager(layoutmanager);
+            ReceptionHistoryAdapter receptionHistoryAdapter = new ReceptionHistoryAdapter(this, mReceptionInfoList);
+            recyclerView.setAdapter(receptionHistoryAdapter);
+        } catch (JSONException e) {
+            Logger.i("TEST","JSONException: "+e);
+
+        }
 
 
     }
---
+++ app/src/main/java/com/yijian/staff/mvp/reception/ReceptionHistoryAdapter.java
@@ -0,0 +1,91 @@
+package com.yijian.staff.mvp.reception;
+
+import android.content.Context;
+import android.media.Image;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.yijian.staff.R;
+import com.yijian.staff.util.Logger;
+
+import java.util.List;
+
+/**
+ * author：李帅华
+ * email：850716183@qq.com
+ * time: 2018/2/28 17:08:17
+ */
+public class ReceptionHistoryAdapter extends RecyclerView.Adapter<ReceptionHistoryAdapter.ViewHolder> {
+    private List<ReceptionInfo> mReceptionInfoList;
+    private Context context;
+
+    public ReceptionHistoryAdapter(Context context, List<ReceptionInfo> mReceptionInfoList) {
+        this.mReceptionInfoList = mReceptionInfoList;
+        this.context = context;
+    }
+
+    @Override
+    public ReceptionHistoryAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_reception_history, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ReceptionHistoryAdapter.ViewHolder holder, int position) {
+        Logger.i("ReceptionHistoryAdapter","position: "+position);
+        ReceptionInfo receptionInfo = mReceptionInfoList.get(position);
+        holder.viperName.setText(receptionInfo.getName());
+        holder.receptionStatus.setText(receptionInfo.getStatus());
+        holder.viperPhone.setText(receptionInfo.getPhone());
+        holder.product.setText(receptionInfo.getProduct());
+        Glide.with(context).load(R.mipmap.lg_man).into(holder.viperSex);
+        holder.wenJuan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        holder.tiCeBaoGao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mReceptionInfoList.size();
+    }
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView viperName;
+        ImageView viperSex;
+        TextView receptionStatus;
+        TextView viperPhone;
+        TextView wenJuan;
+        TextView tiCeBaoGao;
+        TextView product;
+
+        public ViewHolder(View view) {
+            super(view);
+            viperName=     view.findViewById(R.id.tv_viper_name);
+            viperSex=     view.findViewById(R.id.iv_sex);
+            receptionStatus=     view.findViewById(R.id.tv_status);
+            viperPhone=     view.findViewById(R.id.tv_viper_phone);
+            product=     view.findViewById(R.id.tv_product);
+            wenJuan=     view.findViewById(R.id.tv_wenjuan);
+            tiCeBaoGao=     view.findViewById(R.id.tv_ticebaogao);
+
+        }
+    }
+}

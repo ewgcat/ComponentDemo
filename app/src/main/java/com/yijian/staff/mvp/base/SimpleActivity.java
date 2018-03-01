df1c8223a5b16077c456b0b4965bd1b2ae738f94
lishuaihua <lishuaihua@ejoyst.com>
2018-03-01 10:42:22 +0800

    1

--- app/src/main/java/com/yijian/staff/mvp/base/SimpleActivity.java
+++ app/src/main/java/com/yijian/staff/mvp/base/SimpleActivity.java
@@ -3,9 +3,12 @@ package com.yijian.staff.mvp.base;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.jaeger.library.StatusBarUtil;
+import com.yijian.staff.R;
 import com.yijian.staff.application.CustomApplication;
 
 import butterknife.ButterKnife;
@@ -16,7 +19,7 @@ import me.yokeyword.fragmentation.SupportActivity;
  * 无MVP的activity基类
  */
 
-public abstract class SimpleActivity extends SupportActivity {
+public abstract class SimpleActivity extends AppCompatActivity {
 
     protected Activity mContext;
     private Unbinder mUnBinder;
@@ -24,6 +27,7 @@ public abstract class SimpleActivity extends SupportActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(getLayout());
         mUnBinder = ButterKnife.bind(this);
         mContext = this;
@@ -32,18 +36,6 @@ public abstract class SimpleActivity extends SupportActivity {
         initEventAndData();
     }
 
-    protected void setToolBar(Toolbar toolbar, String title) {
-        toolbar.setTitle(title);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressedSupport();
-            }
-        });
-    }
 
     protected void onViewCreated() {
 

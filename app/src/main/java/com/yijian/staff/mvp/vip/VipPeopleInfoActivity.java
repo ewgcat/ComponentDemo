c6936efa792a1cf45c975ed57af838838fd872a7
yangke <yangke@ejoyst.com>
2018-03-05 17:04:32 +0800

    12

---
+++ app/src/main/java/com/yijian/staff/mvp/vip/VipPeopleInfoActivity.java
@@ -0,0 +1,13 @@
+package com.yijian.staff.mvp.vip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VipPeopleInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vip_people_info);
+    }
+}

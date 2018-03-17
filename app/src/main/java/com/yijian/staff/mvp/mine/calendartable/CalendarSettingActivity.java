6fc2b3eb0edf3532dd95df5fc6c30f8b040efde5
yangke <yangke@ejoyst.com>
2018-03-17 20:04:19 +0800

    添加资格证书的查询和编辑，二维码

---
+++ app/src/main/java/com/yijian/staff/mvp/mine/calendartable/CalendarSettingActivity.java
@@ -0,0 +1,175 @@
+package com.yijian.staff.mvp.mine.calendartable;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.yijian.staff.R;
+import com.yijian.staff.widget.NavigationBar;
+import com.yijian.staff.widget.NavigationBarItemFactory;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.Executors;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CalendarSettingActivity extends AppCompatActivity {
+
+    @BindView(R.id.calendarView)
+    MaterialCalendarView materialCalendarView;//布局内的控件
+
+    List<CalendarDay> hasClassList; //当日有课日期集合
+    List<CalendarDay> disableAppointmentList; //不可预约日期集合
+    boolean isEdit = false;  //是否可编辑的状态
+    AllNoneSelectedDecorator allNoneSelectedDecorator;//所有都不可编辑
+    DisableAppointDecorator disableAppointDecorator;//设置不可预约状态
+    EventDecorator eventDecorator; //设置小红点
+    List<CalendarDay> calendarDayList; //装载选中的日程集合
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_setting);
+        ButterKnife.bind(this);
+        initTitle();
+        initData();
+        initView();
+    }
+
+    private void initData() {
+        calendarDayList = new ArrayList<CalendarDay>();
+
+        hasClassList = new ArrayList<CalendarDay>();
+        CalendarDay day = CalendarDay.from(2018, 2, 7);
+        CalendarDay day2 = CalendarDay.from(2018, 2, 8);
+        hasClassList.add(day);
+        hasClassList.add(day2);
+
+        disableAppointmentList = new ArrayList<CalendarDay>();
+        CalendarDay day3 = CalendarDay.from(2018, 2, 13);
+        CalendarDay day4 = CalendarDay.from(2018, 2, 14);
+        CalendarDay day5 = CalendarDay.from(2018, 2, 15);
+        disableAppointmentList.add(day3);
+        disableAppointmentList.add(day4);
+        disableAppointmentList.add(day5);
+
+        allNoneSelectedDecorator = new AllNoneSelectedDecorator();
+        disableAppointDecorator = new DisableAppointDecorator();
+        eventDecorator = new EventDecorator(Color.RED, hasClassList);
+    }
+
+    private void initView() {
+        Calendar instance = Calendar.getInstance();
+//        materialCalendarView.setSelectedDate(instance.getTime());
+        materialCalendarView.setSelectionMode(MaterialCalendarView.SELECTION_MODE_MULTIPLE); //设置多选
+        //设置多个不可选日期的颜色
+        materialCalendarView.addDecorator(disableAppointDecorator);
+        materialCalendarView.addDecorator(eventDecorator);
+        materialCalendarView.addDecorator(allNoneSelectedDecorator);
+
+
+        materialCalendarView.invalidateDecorators();
+        //编辑日历属性
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.MONDAY)   //设置每周开始的第一天
+                .setMinimumDate(CalendarDay.from(2010, 4, 3))  //设置可以显示的最早时间
+                .setMaximumDate(CalendarDay.from(2020, 5, 12))//设置可以显示的最晚时间
+                .setCalendarDisplayMode(CalendarMode.MONTHS)//设置显示模式，可以显示月的模式，也可以显示周的模式
+                .commit();// 返回对象并保存
+
+    }
+
+    private void initTitle() {
+        NavigationBar navigationBar = (NavigationBar) findViewById(R.id.reception_activity_navigation_bar);
+        navigationBar.setTitle("日程表设置", "#ffffff");
+        navigationBar.getmRightTextView().setText("编辑");
+        navigationBar.hideBottomLine();
+        navigationBar.setLeftButtonView(NavigationBarItemFactory.createNavigationItemImageView(this, NavigationBarItemFactory.NavigationItemType.BACK_WHITE));
+        navigationBar.setLeftButtonClickListener(NavigationBarItemFactory.createBackClickListener(this));
+        navigationBar.setRightButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isEdit){ //isEdit false : 不可编辑，true  ： 可编辑（完成）
+                     materialCalendarView.removeDecorator(allNoneSelectedDecorator);
+                    navigationBar.getmRightTextView().setText("完成");
+                }else{ //点击完成 在此处做逻辑处理
+                    navigationBar.getmRightTextView().setText("编辑");
+
+                    calendarDayList = materialCalendarView.getSelectedDates();
+                    for(CalendarDay calendarDay : calendarDayList){
+                        Log.e("Test","年份==="+calendarDay.getYear()+"  月份=="+(calendarDay.getMonth()+1)+" 日=="+calendarDay.getDay());
+                    }
+//                    materialCalendarView.clearSelection(); //清除掉所有选中日程
+                    materialCalendarView.addDecorator(allNoneSelectedDecorator);
+                }
+                isEdit = !isEdit;
+            }
+        });
+    }
+
+    /**
+     * 设置不可预约的状态
+     */
+    private class DisableAppointDecorator implements DayViewDecorator {
+
+        private final Drawable backgroundDrawable;
+
+        public DisableAppointDecorator() {
+            backgroundDrawable = getResources().getDrawable(R.mipmap.my_circle_red_big);
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+
+            boolean flag = false;
+            for(CalendarDay calendarDay : disableAppointmentList){
+                if(day.equals(calendarDay)){
+                    flag = true;
+                }
+            }
+
+            return flag;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.setDaysDisabled(true);
+            view.setBackgroundDrawable(backgroundDrawable);
+        }
+    }
+
+    /**
+     * 设置所有选项都不可编辑
+     */
+    private class AllNoneSelectedDecorator implements DayViewDecorator {
+
+        public AllNoneSelectedDecorator() {
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            return true;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.setDaysDisabled(true);
+        }
+    }
+
+
+
+}

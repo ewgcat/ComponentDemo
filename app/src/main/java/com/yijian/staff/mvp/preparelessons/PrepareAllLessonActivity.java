9f9dbe69416b0f3f5a3d66421ce45bce3ccb105d
yangke <yangke@ejoyst.com>
2018-03-26 20:36:32 +0800

    修改会籍教练过期会员

---
+++ app/src/main/java/com/yijian/staff/mvp/preparelessons/PrepareAllLessonActivity.java
@@ -0,0 +1,15 @@
+package com.yijian.staff.mvp.preparelessons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yijian.staff.R;
+
+public class PrepareAllLessonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prepare_all_lesson);
+    }
+}
--- app/src/main/java/com/yijian/staff/mvp/preparelessons/PrepareLessonsActivity.java
+++ app/src/main/java/com/yijian/staff/mvp/preparelessons/PrepareLessonsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
@@ -35,13 +36,16 @@ public class PrepareLessonsActivity extends AppCompatActivity {
         navigationBar.setLeftButtonClickListener(NavigationBarItemFactory.createBackClickListener(this));
     }
 
-    @OnClick({R.id.rel_create})
-    public void click(View v){
-        switch(v.getId()){
+    @OnClick({R.id.rel_create, R.id.rel_all_lesson})
+    public void click(View v) {
+        switch (v.getId()) {
             case R.id.rel_create: //创建私教备课
-                startActivity(new Intent(PrepareLessonsActivity.this,CreatePrivateLessionActivity.class));
+                startActivity(new Intent(PrepareLessonsActivity.this, CreatePrivateLessionActivity.class));
                 break;
 
+            case R.id.rel_all_lesson:
+                startActivity(new Intent(PrepareLessonsActivity.this, PrepareAllLessonActivity.class));
+                break;
         }
     }
 

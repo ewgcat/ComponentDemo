df7484214b6499c2fe711528f795661b7708131c
lishuaihua <lishuaihua@ejoyst.com>
2018-03-27 19:37:35 +0800

    教练体验课

--- app/src/main/java/com/yijian/staff/mvp/login/LoginActivity.java
+++ app/src/main/java/com/yijian/staff/mvp/login/LoginActivity.java
@@ -66,38 +66,38 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (id) {
             case R.id.ll_login:
 
-                if (TextUtils.isEmpty(account) || TextUtils.isEmpty(password)) {
-                    Toast.makeText(LoginActivity.this, "账号和密码不能为空", Toast.LENGTH_SHORT).show();
-                    return;
-                } else {
-                    LoginRequestBody loginRequest = new LoginRequestBody(account, password);
-                    HttpManager.postLogin(loginRequest, new ResultObserver() {
-                        @Override
-                        public void onSuccess(JSONObject result) {
+//                if (TextUtils.isEmpty(account) || TextUtils.isEmpty(password)) {
+//                    Toast.makeText(LoginActivity.this, "账号和密码不能为空", Toast.LENGTH_SHORT).show();
+//                    return;
+//                } else {
+//                    LoginRequestBody loginRequest = new LoginRequestBody(account, password);
+//                    HttpManager.postLogin(loginRequest, new ResultObserver() {
+//                        @Override
+//                        public void onSuccess(JSONObject result) {
+//
+//                            User user = new User(result);
+//                            SharePreferenceUtil.setUserId(user.getUserId());
+//                            SharePreferenceUtil.setUserRole(user.getRole());
+//
+//                            DBManager.getInstance().insertOrReplaceUser(user);
+//                            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+//                            startActivity(i);
+//                            finish();
+//
+//
+//                        }
+//
+//                        @Override
+//                        public void onFail(String msg) {
+//                            Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
 
-                            User user = new User(result);
-                            SharePreferenceUtil.setUserId(user.getUserId());
-                            SharePreferenceUtil.setUserRole(user.getRole());
-
-                            DBManager.getInstance().insertOrReplaceUser(user);
-                            Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(i);
-                            finish();
+//                        }
+//                    });
 
 
-                        }
+//                }
 
-                        @Override
-                        public void onFail(String msg) {
-                            Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
-
-                        }
-                    });
-
-
-                }
-
-                SharePreferenceUtil.setUserRole(2);
+                SharePreferenceUtil.setUserRole(1);
                 Intent i = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(i);
                 finish();

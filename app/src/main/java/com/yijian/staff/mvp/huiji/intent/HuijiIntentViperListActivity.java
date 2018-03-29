ae263656f661fcfb1e19f8629050ee72a7f04484
yangke <yangke@ejoyst.com>
2018-03-29 10:22:57 +0800

    修改会籍教练过期会员

--- app/src/main/java/com/yijian/staff/mvp/huiji/intent/HuijiIntentViperListActivity.java
+++ app/src/main/java/com/yijian/staff/mvp/huiji/intent/HuijiIntentViperListActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
@@ -16,13 +17,22 @@ import com.scwang.smartrefresh.layout.header.BezierRadarHeader;
 import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener;
 import com.yijian.staff.R;
 import com.yijian.staff.bean.ViperBean;
+import com.yijian.staff.db.DBManager;
+import com.yijian.staff.db.bean.User;
+import com.yijian.staff.mvp.huiji.outdate.HuijiOutdateViperListActivity;
+import com.yijian.staff.mvp.vip.bean.VipOutdateInfo;
+import com.yijian.staff.net.httpmanager.HttpManager;
+import com.yijian.staff.net.response.ResultObserver;
+import com.yijian.staff.util.JsonUtil;
 import com.yijian.staff.util.Logger;
 import com.yijian.staff.widget.NavigationBar2;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import butterknife.BindView;
@@ -39,7 +49,11 @@ public class HuijiIntentViperListActivity extends AppCompatActivity  {
     @BindView(R.id.rv_vip_intention)
     RecyclerView rv_vip_intention;
 
-    private List<ViperBean> viperBeanList=new ArrayList<>();
+    private HuijiIntentViperListAdapter huijiIntentViperListAdapter;
+    private List<ViperBean> viperBeanList = new ArrayList<>();
+    private int pageNum = 1;//页码
+    private int pageSize = 1;//每页数量
+    private int pages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,22 +61,29 @@ public class HuijiIntentViperListActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_intent_viper_list);
         ButterKnife.bind(this);
 
+        initTitle();
         initView();
-        initVipPeopleList();
+//        initVipPeopleList();
     }
 
-    private void initView() {
-
+    private void initTitle() {
         NavigationBar2 navigationBar2 = findViewById(R.id.vip_intent_navigation_bar);
         navigationBar2.hideBottomLine();
         navigationBar2.hideLeftSecondIv();
         navigationBar2.setBackClickListener(this);
         navigationBar2.setTitle("意向会员");
+    }
 
+    private void initView(){
+        LinearLayoutManager layoutmanager = new LinearLayoutManager(this);
+        //设置RecyclerView 布局
+        rv_vip_intention.setLayoutManager(layoutmanager);
+        huijiIntentViperListAdapter = new HuijiIntentViperListAdapter(this, viperBeanList);
+        rv_vip_intention.setAdapter(huijiIntentViperListAdapter);
         initComponent();
     }
 
-    private void initVipPeopleList(){
+    /*private void initVipPeopleList(){
         JSONObject jsonObject = new JSONObject();
         try {
             jsonObject.put("headerUrl", "");
@@ -84,35 +105,119 @@ public class HuijiIntentViperListActivity extends AppCompatActivity  {
             LinearLayoutManager layoutmanager = new LinearLayoutManager(this);
             //设置RecyclerView 布局
             rv_vip_intention.setLayoutManager(layoutmanager);
-            HuijiIntentViperListAdapter huijiIntentViperListAdapter = new HuijiIntentViperListAdapter(this, viperBeanList);
+            huijiIntentViperListAdapter = new HuijiIntentViperListAdapter(this, viperBeanList);
             rv_vip_intention.setAdapter(huijiIntentViperListAdapter);
         } catch (JSONException e) {
             Logger.i("TEST", "JSONException: " + e);
 
         }
-    }
+    }*/
 
 
 
 
     public void initComponent() {
         //设置 Header 为 BezierRadar 样式
-        BezierRadarHeader header = new BezierRadarHeader(HuijiIntentViperListActivity.this).setEnableHorizontalDrag(true);
+        BezierRadarHeader header = new BezierRadarHeader(this).setEnableHorizontalDrag(true);
         header.setPrimaryColor(Color.parseColor("#1997f8"));
         refreshLayout.setRefreshHeader(header);
         //设置 Footer 为 球脉冲
-        BallPulseFooter footer = new BallPulseFooter(HuijiIntentViperListActivity.this).setSpinnerStyle(SpinnerStyle.Scale);
+        BallPulseFooter footer = new BallPulseFooter(this).setSpinnerStyle(SpinnerStyle.Scale);
         footer.setAnimatingColor(Color.parseColor("#1997f8"));
         refreshLayout.setRefreshFooter(footer);
         refreshLayout.setOnRefreshLoadMoreListener(new OnRefreshLoadMoreListener() {
             @Override
             public void onRefresh(@NonNull RefreshLayout refreshLayout) {
-                refreshLayout.finishRefresh(2000/*,false*/);//传入false表示刷新失败
+                refresh();
             }
+
             @Override
             public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
-                refreshLayout.finishLoadMore(2000/*,false*/);//传入false表示刷新失败
+                loadMore();
             }
         });
     }
+
+
+    private void refresh() {
+        HashMap<String, String> header = new HashMap<>();
+        User user = DBManager.getInstance().queryUser();
+        header.put("token", user.getToken());
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("pageNum", 1 + "");
+        map.put("pageSize", 1 + "");
+
+        HttpManager.getHasHeaderHasParam(HttpManager.GET_INTENT_VIPER_LIST_URL,header, map, new ResultObserver() {
+            @Override
+            public void onSuccess(JSONObject result) {
+                refreshLayout.finishRefresh(2000, true);
+
+                viperBeanList.clear();
+                pageNum = JsonUtil.getInt(result, "pageNum") + 1;
+                pages = JsonUtil.getInt(result, "pages");
+                JSONArray records = JsonUtil.getJsonArray(result, "records");
+                for (int i = 0; i < records.length(); i++) {
+                    try {
+                        JSONObject jsonObject = (JSONObject) records.get(i);
+                        ViperBean viperBean = new ViperBean(jsonObject);
+                        viperBeanList.add(viperBean);
+                    } catch (JSONException e) {
+
+
+                    }
+                }
+                huijiIntentViperListAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFail(String msg) {
+                refreshLayout.finishRefresh(2000, false);//传入false表示刷新失败
+                Toast.makeText(HuijiIntentViperListActivity.this,msg,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    public void loadMore() {
+        HashMap<String, String> header = new HashMap<>();
+        User user = DBManager.getInstance().queryUser();
+        header.put("token", user.getToken());
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("pageNum", pageNum + "");
+        map.put("pageSize", pageSize + "");
+
+        HttpManager.getAllViperList(header, map, new ResultObserver() {
+            @Override
+            public void onSuccess(JSONObject result) {
+
+                pageNum = JsonUtil.getInt(result, "pageNum") + 1;
+                pages = JsonUtil.getInt(result, "pages");
+
+                boolean hasMore = pages > pageNum ? true : false;
+                refreshLayout.finishLoadMore(2000, true, hasMore);//传入false表示刷新失败
+
+                JSONArray records = JsonUtil.getJsonArray(result, "records");
+                for (int i = 0; i < records.length(); i++) {
+                    try {
+                        JSONObject jsonObject = (JSONObject) records.get(i);
+                        ViperBean viperBean = new ViperBean(jsonObject);
+                        viperBeanList.add(viperBean);
+                    } catch (JSONException e) {
+                    }
+                }
+                huijiIntentViperListAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFail(String msg) {
+                boolean hasMore = pages > pageNum ? true : false;
+                refreshLayout.finishLoadMore(2000, false, hasMore);//传入false表示刷新失败
+                Toast.makeText(HuijiIntentViperListActivity.this,msg,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 }

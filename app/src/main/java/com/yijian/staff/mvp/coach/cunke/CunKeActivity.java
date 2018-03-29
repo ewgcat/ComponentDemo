6512a8ff906fb3da63941e616d7cc013294a68a0
yangke <yangke@ejoyst.com>
2018-03-29 11:00:01 +0800

    sds

--- app/src/main/java/com/yijian/staff/mvp/coach/cunke/CunKeActivity.java
+++ app/src/main/java/com/yijian/staff/mvp/coach/cunke/CunKeActivity.java
@@ -2,23 +2,54 @@ package com.yijian.staff.mvp.coach.cunke;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.yijian.staff.R;
+import com.yijian.staff.mvp.setclass.AdapterLesson;
+import com.yijian.staff.widget.MDividerItemDecoration;
 import com.yijian.staff.widget.NavigationBar2;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 @Route(path = "/test/16")
 public class CunKeActivity extends AppCompatActivity {
+
+    @BindView(R.id.rc_ck)
+    RecyclerView rc_ck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cun_ke);
+        ButterKnife.bind(this);
 
+        initTitle();
+        initView();
+    }
+
+    private void initTitle(){
         NavigationBar2   navigationBar2 = (NavigationBar2) findViewById(R.id.cun_ke_navigation_bar2);
         navigationBar2.hideLeftSecondIv();
         navigationBar2.setBackClickListener(this);
         navigationBar2.setTitle("私教课");
-
     }
+
+    private void initView(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        rc_ck.setLayoutManager(linearLayoutManager);
+        //添加Android自带的分割线
+        MDividerItemDecoration decor = new MDividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+
+       /* decor.setDrawable(getDrawable(R.drawable.divider_recyclerview));
+        rc_ck.addItemDecoration(decor);
+        rc_ck.setNestedScrollingEnabled(false);
+        unKeAdapter = new CuncKeAdapter(this);
+        rc_ck.setAdapter(adapterLesson);*/
+    }
+
 }
---
+++ app/src/main/java/com/yijian/staff/mvp/coach/cunke/CunKeAdapter.java
@@ -0,0 +1,41 @@
+package com.yijian.staff.mvp.coach.cunke;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yangk on 2018/3/29.
+ */
+
+public class CunKeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+   /* public CuncKeAdapter(){
+
+    }*/
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class TitleViewHolder extends RecyclerView.ViewHolder{
+
+
+
+        public TitleViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+}

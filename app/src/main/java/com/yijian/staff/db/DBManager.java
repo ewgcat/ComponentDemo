4e7ccc9e88944eb3483343e7b4e439f62391a8e6
lishuaihua <lishuaihua@ejoyst.com>
2018-04-09 15:09:31 +0800

    更换搜索图标

--- app/src/main/java/com/yijian/staff/db/DBManager.java
+++ app/src/main/java/com/yijian/staff/db/DBManager.java
@@ -7,6 +7,7 @@ import com.yijian.staff.db.bean.SearchKey;
 import com.yijian.staff.db.bean.User;
 import com.yijian.staff.greendao.gen.DaoMaster;
 import com.yijian.staff.greendao.gen.DaoSession;
+import com.yijian.staff.greendao.gen.SearchKeyDao;
 import com.yijian.staff.greendao.gen.UserDao;
 import com.yijian.staff.prefs.SharePreferenceUtil;
 

d1948d3171b8200b9961a7209ed7b1eb620052ce
lishuaihua <lishuaihua@ejoyst.com>
2018-04-24 10:34:22 +0800

    更改user表格对象，移除无用文件

--- app/src/main/java/com/yijian/staff/mvp/login/LoginActivity.java
+++ app/src/main/java/com/yijian/staff/mvp/login/LoginActivity.java
@@ -14,6 +14,7 @@ import com.alibaba.android.arouter.facade.annotation.Route;
 import com.yijian.staff.R;
 import com.yijian.staff.db.DBManager;
 import com.yijian.staff.db.bean.User;
+import com.yijian.staff.mvp.base.mvc.MvcBaseActivity;
 import com.yijian.staff.mvp.forgetpassword.ForgetPasswordActivity;
 import com.yijian.staff.mvp.main.MainActivity;
 import com.yijian.staff.net.requestbody.login.LoginRequestBody;
@@ -25,22 +26,21 @@ import com.yijian.staff.util.CommonUtil;
 import org.json.JSONObject;
 
 @Route(path = "/test/login")
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends MvcBaseActivity implements View.OnClickListener {
 
 
     private EditText etAccount;
     private EditText etPassword;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_login);
-        initView();
+    @Override
+    protected int getLayoutID() {
+        return R.layout.activity_login;
     }
 
-    protected void initView() {
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         etAccount = findViewById(R.id.et_account);
         etPassword = findViewById(R.id.et_password);
         etAccount.setText(SharePreferenceUtil.getUserName());
@@ -51,58 +51,55 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         findViewById(R.id.forget_password).setOnClickListener(this);
     }
 
+
     @Override
     public void onClick(View v) {
-        int id = v.getId();
-        String account = etAccount.getText().toString();
-        String password = etPassword.getText().toString();
-
-
-        switch (id) {
+        switch (v.getId()) {
             case R.id.ll_login:
-
-                if (TextUtils.isEmpty(account) || TextUtils.isEmpty(password)) {
-                    Toast.makeText(LoginActivity.this, "账号和密码不能为空", Toast.LENGTH_SHORT).show();
-                    return;
-                } else {
-                    if (CommonUtil.isPassWordFormat(password)){
-                        LoginRequestBody loginRequest = new LoginRequestBody(account, password);
-                        HttpManager.postLogin(loginRequest, new ResultJSONObjectObserver() {
-                            @Override
-                            public void onSuccess(JSONObject result) {
-
-                                User user = new User(result);
-                                SharePreferenceUtil.setUserName(account);
-                                SharePreferenceUtil.setUserId(user.getUserId());
-                                SharePreferenceUtil.setUserRole(user.getRole());
-
-                                DBManager.getInstance().insertOrReplaceUser(user);
-                                Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                                startActivity(i);
-                                finish();
-                            }
-
-                            @Override
-                            public void onFail(String msg) {
-                                Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
-
-                            }
-                        });
-                    }else {
-                        Toast.makeText(LoginActivity.this, "密码格式错误，密码是数字和字母的6-20位组合！", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                }
-
-
+                login();
                 break;
             case R.id.forget_password:
-                //TODO 忘记密码
-                Intent intent = new Intent(LoginActivity.this, ForgetPasswordActivity.class);
-                intent.putExtra("account", account);
-                startActivity(intent);
-
+                jumpToForgetPassword();
                 break;
+        }
+    }
+
+    private void jumpToForgetPassword() {
+        String account = etAccount.getText().toString();
+        Intent intent = new Intent(LoginActivity.this, ForgetPasswordActivity.class);
+        intent.putExtra("account", account);
+        startActivity(intent);
+    }
+
+    private void login() {
+        String account = etAccount.getText().toString();
+        String password = etPassword.getText().toString();
+        if (TextUtils.isEmpty(account) || TextUtils.isEmpty(password)) {
+            showToast("账号和密码不能为空");
+        } else {
+            if (CommonUtil.isPassWordFormat(password)) {
+                LoginRequestBody loginRequest = new LoginRequestBody(account, password);
+                HttpManager.postLogin(loginRequest, new ResultJSONObjectObserver() {
+                    @Override
+                    public void onSuccess(JSONObject result) {
+                        User user = new User(result);
+                        SharePreferenceUtil.setUserName(account);
+                        SharePreferenceUtil.setUserId(user.getUserId());
+                        SharePreferenceUtil.setUserRole(user.getRole());
+                        DBManager.getInstance().insertOrReplaceUser(user);
+                        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+
+                    @Override
+                    public void onFail(String msg) {
+                        showToast(msg);
+                    }
+                });
+            } else {
+                showToast("密码格式错误，密码是数字和字母的6-20位组合！");
+            }
         }
     }
 

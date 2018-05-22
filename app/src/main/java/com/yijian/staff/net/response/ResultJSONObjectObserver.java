01628f0e8738be661cef6a7b4c4a1d55583b05d5
huangrifu <498963023@qq.com>
2018-05-22 11:06:14 +0800

    1.修改 resultJsonObjectOberser

--- app/src/main/java/com/yijian/staff/net/response/ResultJSONObjectObserver.java
+++ app/src/main/java/com/yijian/staff/net/response/ResultJSONObjectObserver.java
@@ -1,71 +1,21 @@
 package com.yijian.staff.net.response;
 
 
-import android.util.Log;
-
-import com.alibaba.android.arouter.launcher.ARouter;
-import com.yijian.staff.util.JsonUtil;
-import com.yijian.staff.util.Logger;
-
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import io.reactivex.Observer;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
 
-
-public abstract  class ResultJSONObjectObserver implements Observer<JSONObject>   , ResultCallBack<JSONObject>{
-
-    private Disposable mDisposable;
-
+public abstract class ResultJSONObjectObserver extends ResponseObserver<JSONObject> {
 
     public ResultJSONObjectObserver() {
+        super();
+    }
 
+    protected void initResultType() {
+        dataClassType = JSONObject.class;
     }
 
     @Override
-    public void onSubscribe(@NonNull Disposable disposable) {
-        mDisposable = disposable;
-    }
-
-    @Override
-    public void onNext(JSONObject jsonObject) {
-        Logger.i("Result",jsonObject.toString());
-        try {
-            int code = jsonObject.getInt("code");
-            if (code==0){
-                JSONObject data = JsonUtil.getJsonObject(jsonObject,"data");
-                if (data!=null){
-                    onSuccess(data);
-                }else {
-                    onSuccess(new JSONObject());
-                }
-            }else if (code==3){
-                String msg = jsonObject.getString("msg");
-                onFail(msg);
-                ARouter.getInstance().build("/test/login").navigation();
-            }else {
-                String msg = jsonObject.getString("msg");
-                onFail(msg);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onError(Throwable e) {
-        onFail(e.getMessage());
-        Log.i("Result", "onError "+e.toString());
-    }
-
-    @Override
-    public void onComplete() {
-        Log.i("Result", "onCompleted==请求结束");
-    }
-
-    public Disposable getmDisposable() {
-        return mDisposable;
+    protected void responData(JSONObject jsonObject) throws Exception {
+        onSuccess(jsonObject.getJSONObject("data"));
     }
 }

1e6add7cfb67fb80e91a99b60e3de9c5c0c30c45
yangke <yangke@ejoyst.com>
2018-05-22 15:10:33 +0800

    修改会籍赛选

--- app/src/main/java/com/yijian/staff/mvp/main/work/face2/FaceDetectorActivity.java
+++ app/src/main/java/com/yijian/staff/mvp/main/work/face2/FaceDetectorActivity.java
@@ -195,7 +195,7 @@ public class FaceDetectorActivity extends AppCompatActivity implements Camera.Pr
                 // mCamera.setPreviewCallback(null);// 防止 Method called after release()
                 if (mCamera != null) {
                     mCamera.stopPreview();
-                    // mCamera.setPreviewCallback(null);
+                     mCamera.setPreviewCallback(null);
                     mCamera.release();
                     mCamera = null;
                     holder = null;
@@ -540,10 +540,72 @@ public class FaceDetectorActivity extends AppCompatActivity implements Camera.Pr
     }*/
 
 
+    //上次记录的时间戳
+    long lastRecordTime = System.currentTimeMillis();
+
+    //上次记录的索引
+    int darkIndex = 0;
+    //一个历史记录的数组，255是代表亮度最大值
+    long[] darkList = new long[]{255, 255, 255, 255};
+    //扫描间隔
+    int waitScanTime = 300;
+
+    //亮度低的阀值
+    int darkValue = 60;
+
     @Override
     public void onPreviewFrame(byte[] data, Camera camera) {
 //        Log.e("Test","onPreview....."+data.length);
 //        setCameraDisplayOrientation(this, Camera.CameraInfo.CAMERA_FACING_BACK,camera);
+
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastRecordTime < waitScanTime) {
+            return;
+        }
+        lastRecordTime = currentTime;
+
+        int width = camera.getParameters().getPreviewSize().width;
+        int height = camera.getParameters().getPreviewSize().height;
+        //像素点的总亮度
+        long pixelLightCount = 0L;
+        //像素点的总数
+        long pixeCount = width * height;
+        //采集步长，因为没有必要每个像素点都采集，可以跨一段采集一个，减少计算负担，必须大于等于1。
+        int step = 10;
+        //data.length - allCount * 1.5f的目的是判断图像格式是不是YUV420格式，只有是这种格式才相等
+        //因为int整形与float浮点直接比较会出问题，所以这么比
+        if (Math.abs(data.length - pixeCount * 1.5f) < 0.00001f) {
+            for (int i = 0; i < pixeCount; i += step) {
+                //如果直接加是不行的，因为data[i]记录的是色值并不是数值，byte的范围是+127到—128，
+                // 而亮度FFFFFF是11111111是-127，所以这里需要先转为无符号unsigned long参考Byte.toUnsignedLong()
+                pixelLightCount += ((long) data[i]) & 0xffL;
+            }
+            //平均亮度
+            long cameraLight = pixelLightCount / (pixeCount / step);
+            //更新历史记录
+            int lightSize = darkList.length;
+            darkList[darkIndex = darkIndex % lightSize] = cameraLight;
+            darkIndex++;
+            boolean isDarkEnv = true;
+            //判断在时间范围waitScanTime * lightSize内是不是亮度过暗
+            for (int i = 0; i < lightSize; i++) {
+                if (darkList[i] > darkValue) {
+                    isDarkEnv = false;
+                }
+            }
+            Log.e(TAG, "摄像头环境亮度为 ： " + cameraLight);
+            if (!isFinishing()) {
+                //亮度过暗就提醒
+                if (isDarkEnv) {
+                    Log.e(TAG,"亮度还行");
+                } else {
+                    Log.e(TAG,"亮度过低");
+                }
+            }
+        }
+
+
+
     }
 
     /**
@@ -667,8 +729,8 @@ public class FaceDetectorActivity extends AppCompatActivity implements Camera.Pr
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    protected void onStop() {
+        super.onStop();
         if (mCamera != null) {
             mCamera.setPreviewCallback(null);
             mCamera.stopPreview();
@@ -678,19 +740,12 @@ public class FaceDetectorActivity extends AppCompatActivity implements Camera.Pr
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        if (mCamera != null) {
-            mCamera.stopPreview();
-        }
-    }
-
-    @Override
     protected void onRestart() {
         super.onRestart();
         if (mCamera != null) {
             mCamera.setPreviewCallback(this);
             mCamera.startPreview();
+            startFaceDetection();
         }
     }
 }

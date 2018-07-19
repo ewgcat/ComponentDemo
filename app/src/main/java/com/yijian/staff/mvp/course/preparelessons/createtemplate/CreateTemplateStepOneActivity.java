3c2d301e13fa456524b4ca4e324ddf156ca1793d
李帅华 <lishuaihua@ejoyst.com>
2018-07-19 16:39:01 +0800

    新增预发布环境配置train

---
+++ app/src/main/java/com/yijian/staff/mvp/course/preparelessons/createtemplate/CreateTemplateStepOneActivity.java
@@ -0,0 +1,15 @@
+package com.yijian.staff.mvp.course.preparelessons.createtemplate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yijian.staff.R;
+
+public class CreateTemplateStepOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_template_step_one);
+    }
+}

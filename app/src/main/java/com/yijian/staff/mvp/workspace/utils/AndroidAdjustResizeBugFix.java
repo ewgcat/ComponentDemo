87734b8f1466e7d198470e00525b3d04dc7831ee
yangke <yangke@ejoyst.com>
2018-08-07 11:30:03 +0800

    Merge remote-tracking branch 'origin/Bapp1.3.0' into YiJian1.1.0-workspace
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/yijian/staff/net/api/ApiService.java
    #	app/src/main/java/com/yijian/staff/net/httpmanager/HttpManager.java
    #	app/src/main/java/com/yijian/staff/prefs/SharePreferenceUtil.java

---
+++ app/src/main/java/com/yijian/staff/mvp/workspace/utils/AndroidAdjustResizeBugFix.java
@@ -0,0 +1,84 @@
+package com.yijian.staff.mvp.workspace.utils;
+
+import android.app.Activity;
+import android.graphics.Rect;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.lang.reflect.Field;
+
+public class AndroidAdjustResizeBugFix {
+    private View mChildOfContent;
+    private int usableHeightPrevious;
+    private int statusBarHeight;
+    private FrameLayout.LayoutParams frameLayoutParams;
+    private Activity mActivity;
+    private CallKeyBoardStatu listener;
+
+    public void setListener(CallKeyBoardStatu listener) {
+        this.listener = listener;
+    }
+
+    private AndroidAdjustResizeBugFix(Activity activity, CallKeyBoardStatu listener) {
+        mActivity = activity;
+        this.listener = listener;
+        FrameLayout content = (FrameLayout) activity.findViewById(android.R.id.content);
+        mChildOfContent = content.getChildAt(0);
+        statusBarHeight = getStatusBarHeight();
+        mChildOfContent.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                possiblyResizeChildOfContent();
+            }
+        });
+        frameLayoutParams = (FrameLayout.LayoutParams) mChildOfContent.getLayoutParams();
+    }
+    public static void assistActivity(Activity activity, CallKeyBoardStatu listener) {
+        new AndroidAdjustResizeBugFix(activity, listener);
+    }
+    private void possiblyResizeChildOfContent() {
+        int usableHeightNow = computeUsableHeight();
+        if (usableHeightNow != usableHeightPrevious) {
+            int usableHeightSansKeyboard = mChildOfContent.getRootView().getHeight();
+            int heightDifference = usableHeightSansKeyboard - usableHeightNow;
+            if (heightDifference > (usableHeightSansKeyboard / 4)) {
+                // keyboard probably just became visible
+                // 如果有高度变化，mChildOfContent.requestLayout()之后界面才会重新测量
+                // 这里就随便让原来的高度减去了1
+                frameLayoutParams.height = usableHeightSansKeyboard - 1;
+                listener.callkeyboardstatu(true, heightDifference);
+            } else {
+                // keyboard probably just became hidden
+                frameLayoutParams.height = usableHeightSansKeyboard;
+                listener.callkeyboardstatu(false, heightDifference);
+            }
+            mChildOfContent.requestLayout();
+            usableHeightPrevious = usableHeightNow;
+        }
+    }
+    private int computeUsableHeight() {
+        Rect r = new Rect();
+        mChildOfContent.getWindowVisibleDisplayFrame(r);
+        return r.bottom - r.top + statusBarHeight;
+    }
+    private int getStatusBarHeight() {
+        try {
+            Class<?> c = Class.forName("com.android.internal.R$dimen");
+            Object obj = c.newInstance();
+            Field field = c.getField("status_bar_height");
+            int x = Integer.parseInt(field.get(obj).toString());
+            int dimensionPixelSize = mActivity.getResources().getDimensionPixelSize(x);
+            return dimensionPixelSize;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public interface CallKeyBoardStatu{
+        void callkeyboardstatu(boolean showFlag, int heightDifference);
+    }
+
+}

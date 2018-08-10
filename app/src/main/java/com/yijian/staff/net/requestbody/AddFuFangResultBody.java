bbe19bf2e40484ffe41bcbc10ee54d3b27da35cd
李帅华 <lishuaihua@ejoyst.com>
2018-08-10 16:40:24 +0800

    会籍邀约只能邀约自己的会员

---
+++ app/src/main/java/com/yijian/staff/net/requestbody/AddFuFangResultBody.java
@@ -0,0 +1,54 @@
+package com.yijian.staff.net.requestbody;
+
+/**
+ * author：李帅华
+ * email：850716183@qq.com
+ * time: 2018/8/10 16:08:29
+ */
+public class AddFuFangResultBody {
+
+
+    /**
+     * chief : true
+     * id : string
+     * reviewReason : string
+     * reviewTime : 2018-08-10T06:07:15.995Z
+     */
+
+    private boolean chief;
+    private String id;
+    private String reviewReason;
+    private String reviewTime;
+
+    public boolean isChief() {
+        return chief;
+    }
+
+    public void setChief(boolean chief) {
+        this.chief = chief;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getReviewReason() {
+        return reviewReason;
+    }
+
+    public void setReviewReason(String reviewReason) {
+        this.reviewReason = reviewReason;
+    }
+
+    public String getReviewTime() {
+        return reviewTime;
+    }
+
+    public void setReviewTime(String reviewTime) {
+        this.reviewTime = reviewTime;
+    }
+}

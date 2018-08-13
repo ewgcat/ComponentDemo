b09fa3109041979a2d888ed2e453681759588e6d
李帅华 <lishuaihua@ejoyst.com>
2018-08-13 11:09:56 +0800

    会籍邀约只能邀约自己的会员

---
+++ app/src/main/java/com/yijian/staff/bean/HuifangRecordRequestBody.java
@@ -0,0 +1,39 @@
+package com.yijian.staff.bean;
+
+/**
+ * author：李帅华
+ * email：850716183@qq.com
+ * time: 2018/8/13 09:59:29
+ */
+public class HuifangRecordRequestBody {
+
+
+    private boolean chief;
+    private int pageNum;
+    private int pageSize;
+
+    public boolean isChief() {
+        return chief;
+    }
+
+    public void setChief(boolean chief) {
+        this.chief = chief;
+    }
+
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+}

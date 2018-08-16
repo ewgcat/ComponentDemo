e1347256ec03aa52ec2f66e0da0a6a2ad09e7734
李帅华 <lishuaihua@ejoyst.com>
2018-08-16 11:55:55 +0800

    分包

--- app/src/main/java/com/yijian/staff/mvp/course/yueke/YueKeBiaoActivity.java
+++ app/src/main/java/com/yijian/staff/mvp/course/yueke/YueKeBiaoActivity.java
@@ -1,24 +1,38 @@
 package com.yijian.staff.mvp.course.yueke;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.yijian.staff.R;
 import com.yijian.staff.mvp.base.mvc.MvcBaseActivity;
-import com.yijian.staff.mvp.course.timetable.schedule.day.ScheduleDayFragment;
-import com.yijian.staff.mvp.course.timetable.schedule.week.ScheduleWeekFragment;
 import com.yijian.staff.widget.NavigationBar2;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class YueKeBiaoActivity extends MvcBaseActivity {
 
 
+    @BindView(R.id.ll1)
+    LinearLayout ll1;
+    @BindView(R.id.ll2)
+    LinearLayout ll2;
+    @BindView(R.id.ll3)
+    LinearLayout ll3;
+    @BindView(R.id.ll4)
+    LinearLayout ll4;
+    @BindView(R.id.ll5)
+    LinearLayout ll5;
+    @BindView(R.id.ll6)
+    LinearLayout ll6;
+    @BindView(R.id.ll7)
+    LinearLayout ll7;
     private YueKeBiaoFragment yueKeBiaoFragment1;
     private YueKeBiaoFragment yueKeBiaoFragment2;
     private YueKeBiaoFragment yueKeBiaoFragment3;
@@ -43,34 +57,74 @@ public class YueKeBiaoActivity extends MvcBaseActivity {
 
     }
 
+    public void selectDay(int i) {
+        ll1.setBackgroundColor(Color.parseColor("#f1f2f6"));
+        ll2.setBackgroundColor(Color.parseColor("#f1f2f6"));
+        ll3.setBackgroundColor(Color.parseColor("#f1f2f6"));
+        ll4.setBackgroundColor(Color.parseColor("#f1f2f6"));
+        ll5.setBackgroundColor(Color.parseColor("#f1f2f6"));
+        ll6.setBackgroundColor(Color.parseColor("#f1f2f6"));
+        ll7.setBackgroundColor(Color.parseColor("#f1f2f6"));
+        switch (i) {
+            case 0:
+                ll1.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                break;
+            case 1:
+                ll2.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                break;
+            case 2:
+                ll3.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                break;
+            case 3:
+                ll4.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                break;
+            case 4:
+                ll5.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                break;
+            case 5:
+                ll6.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                break;
+            case 6:
+                ll7.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                break;
+        }
+    }
+
     @OnClick({R.id.ll1, R.id.ll2, R.id.ll3, R.id.ll4, R.id.ll5, R.id.ll6, R.id.ll7})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.ll1:
                 changeFragment(0);
+                selectDay(0);
                 break;
             case R.id.ll2:
                 changeFragment(1);
+                selectDay(1);
 
                 break;
             case R.id.ll3:
                 changeFragment(2);
+                selectDay(2);
 
                 break;
             case R.id.ll4:
                 changeFragment(3);
+                selectDay(3);
 
                 break;
             case R.id.ll5:
                 changeFragment(4);
+                selectDay(4);
 
                 break;
             case R.id.ll6:
                 changeFragment(5);
+                selectDay(5);
 
                 break;
             case R.id.ll7:
                 changeFragment(6);
+                selectDay(6);
 
                 break;
         }
@@ -83,7 +137,7 @@ public class YueKeBiaoActivity extends MvcBaseActivity {
         switch (index) {
             case 0:
                 if (yueKeBiaoFragment1 == null) {
-                     yueKeBiaoFragment1 = new YueKeBiaoFragment();
+                    yueKeBiaoFragment1 = new YueKeBiaoFragment();
                     fragmentTransaction.add(R.id.fl_content, yueKeBiaoFragment1);
                 } else {
                     fragmentTransaction.show(yueKeBiaoFragment1);
@@ -98,7 +152,7 @@ public class YueKeBiaoActivity extends MvcBaseActivity {
                 }
                 break;
             case 2:
-                if (yueKeBiaoFragment3== null) {
+                if (yueKeBiaoFragment3 == null) {
                     yueKeBiaoFragment3 = new YueKeBiaoFragment();
                     fragmentTransaction.add(R.id.fl_content, yueKeBiaoFragment3);
                 } else {
@@ -106,8 +160,8 @@ public class YueKeBiaoActivity extends MvcBaseActivity {
                 }
                 break;
             case 3:
-                if (yueKeBiaoFragment4== null) {
-                    yueKeBiaoFragment4= new YueKeBiaoFragment();
+                if (yueKeBiaoFragment4 == null) {
+                    yueKeBiaoFragment4 = new YueKeBiaoFragment();
                     fragmentTransaction.add(R.id.fl_content, yueKeBiaoFragment4);
                 } else {
                     fragmentTransaction.show(yueKeBiaoFragment4);
@@ -143,7 +197,6 @@ public class YueKeBiaoActivity extends MvcBaseActivity {
     }
 
 
-
     //隐藏所有的Fragment
     public void hideAllIndex(FragmentTransaction fragmentTransaction) {
 
@@ -169,4 +222,6 @@ public class YueKeBiaoActivity extends MvcBaseActivity {
             fragmentTransaction.hide(yueKeBiaoFragment7);
         }
     }
+
+
 }

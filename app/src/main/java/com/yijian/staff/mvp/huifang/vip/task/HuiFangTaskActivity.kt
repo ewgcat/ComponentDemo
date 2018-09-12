f8d28c415bdf2f5739417706fa9c79cb58a5b090
李帅华 <lishuaihua@ejoyst.com>
2018-09-12 19:52:54 +0800

    回访代码java类换成kotlin

---
+++ app/src/main/java/com/yijian/staff/mvp/huifang/vip/task/HuiFangTaskActivity.kt
@@ -0,0 +1,128 @@
+package com.yijian.staff.mvp.huifang.vip.task
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
+import android.view.View
+
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.yijian.staff.R
+import com.yijian.staff.bean.HuiFangTypeBean
+import com.yijian.staff.db.DBManager
+import com.yijian.staff.mvp.base.mvc.MvcBaseActivity
+import com.yijian.staff.mvp.huifang.vip.history.HuiFangHistoryActivity
+import com.yijian.staff.mvp.huifang.vip.task.fragment.BaseHuiFangTaskFragment
+import com.yijian.staff.mvp.huifang.vip.task.pageadapter.HuiFangPagerAdapter
+import com.yijian.staff.net.httpmanager.HttpManager
+import com.yijian.staff.net.requestbody.HuiFangTypeRequestBody
+import com.yijian.staff.net.response.ResultJSONArrayObserver
+import com.yijian.staff.util.JsonUtil
+import com.yijian.staff.widget.NavigationBar
+import com.yijian.staff.widget.PagerSlidingTabStrip
+
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+
+import java.util.ArrayList
+
+import butterknife.BindView
+import butterknife.OnClick
+
+@Route(path = "/test/33")
+class HuiFangTaskActivity : MvcBaseActivity() {
+
+    @BindView(R.id.tabs)
+    lateinit var tabs: PagerSlidingTabStrip
+    @BindView(R.id.viewPager)
+    lateinit var viewPager: ViewPager
+
+    private val huiFangTypeBeanArrayList = ArrayList<HuiFangTypeBean>()
+    private var totalNum: Int = 0
+
+
+    override fun getLayoutID(): Int {
+        return R.layout.activity_hui_fang_task
+    }
+
+    override fun initView(savedInstanceState: Bundle?) {
+        initView()
+    }
+
+    private fun initView() {
+        initNavigation()
+        initData()
+    }
+
+
+    private fun initNavigation() {
+        val navigationBar = findViewById<View>(R.id.hui_fang_task_navigation_bar) as NavigationBar
+        navigationBar.setTitle("会员回访")
+        navigationBar.hideLeftSecondIv()
+        navigationBar.setBackClickListener(this)
+    }
+
+
+    private fun initData() {
+        showLoading()
+        val body = HuiFangTypeRequestBody()
+        body.isChief = true
+        body.type = 0
+        HttpManager.postHuiFangType(body, object : ResultJSONArrayObserver(lifecycle) {
+            override fun onSuccess(result: JSONArray?) {
+                hideLoading()
+                try {
+                    if (result != null && result.length() > 0) {
+                        for (i in 0 until result.length()) {
+                            val jsonObject = result.getJSONObject(i)
+                            val huiFangTypeBean = HuiFangTypeBean(jsonObject)
+                            huiFangTypeBeanArrayList.add(huiFangTypeBean)
+                        }
+                        val jsonObject = result.getJSONObject(0)
+                        if (jsonObject.has("totalNum")) {
+                            totalNum = JsonUtil.getInt(jsonObject, "totalNum")
+                        }
+                        DBManager.getInstance().insertOrReplaceHuiFangTypeBeans(huiFangTypeBeanArrayList)
+                        initIndicatorAndViewPager()
+                    }
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+
+            }
+
+            override fun onFail(msg: String) {
+                hideLoading()
+                showToast(msg)
+            }
+        })
+    }
+
+    private fun initIndicatorAndViewPager() {
+        val mTitleList = ArrayList<String>()
+        val fragmentList = ArrayList<Fragment>()
+        for (i in huiFangTypeBeanArrayList.indices) {
+            val huiFangTypeBean = huiFangTypeBeanArrayList[i]
+            mTitleList.add(huiFangTypeBean.name)
+            fragmentList.add(BaseHuiFangTaskFragment( huiFangTypeBean.menu))
+        }
+        val huiFangPagerAdapter = HuiFangPagerAdapter(supportFragmentManager, fragmentList, mTitleList)
+        viewPager.adapter = huiFangPagerAdapter
+        tabs.setViewPager(viewPager)
+        tabs.updateBubbleNum(0, totalNum)
+        //初始化显示第一页
+        viewPager.currentItem = 0
+    }
+
+
+    @OnClick(R.id.ll_hui_fang_ji_lu)
+    fun onViewClicked(view: View) {
+        when (view.id) {
+            R.id.ll_hui_fang_ji_lu -> startActivity(Intent(this@HuiFangTaskActivity, HuiFangHistoryActivity::class.java))
+        }
+    }
+
+}
+
+
---
+++ app/src/main/java/com/yijian/staff/mvp/huifang/vip/task/pageadapter/HuiFangPagerAdapter.kt
@@ -0,0 +1,46 @@
+package com.yijian.staff.mvp.huifang.vip.task.pageadapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.app.FragmentTransaction
+import android.view.ViewGroup
+
+import java.util.ArrayList
+
+/**
+ * author：李帅华
+ * email：850716183@qq.com
+ * time: 2018/3/5 19:14:06
+ */
+class HuiFangPagerAdapter(private val mFragmentManager: FragmentManager, private val fragments: List<Fragment>, mTitleDataList: List<String>) : FragmentPagerAdapter(mFragmentManager) {
+    private var mTitleDataList = ArrayList<String>()
+    private val fragmentTransaction: FragmentTransaction? = null
+
+
+    init {
+        this.mTitleDataList = mTitleDataList as ArrayList<String>
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return fragments[position]
+    }
+
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mTitleDataList[position]
+    }
+
+    override fun getCount(): Int {
+        return fragments.size
+    }
+
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        return super.instantiateItem(container, position)
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        super.destroyItem(container, position, `object`)
+    }
+}
